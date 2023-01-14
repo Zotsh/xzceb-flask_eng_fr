a175a828276ac64aae27a0f4f6fdcaae02868528
Mduduzi Mlilo <MDUDUZI.MLILO@HOTMAIL.COM>
2023-01-14 14:59:02 +0200

    Htnl render

--- final_project/server.py
+++ final_project/server.py
@@ -4,22 +4,23 @@ import json
 
 app = Flask("Web Translator")
 
+
 @app.route("/english_to_french")
 def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    text_to_translate = request.args.get('textToTranslate')
+    return translator.english_to_french(text_to_translate)
+
 
 @app.route("/french_to_english")
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+def french_to_english():
+    text_to_translate = request.args.get('textToTranslate')
+    return translator.french_to_english(text_to_translate)
+
 
 @app.route("/")
-def renderIndexPage():
-    # Write the code to render template
-    ...
+def render_index_page():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
