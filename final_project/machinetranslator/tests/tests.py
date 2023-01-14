3f6c3068e4164210f53cb405d210b4f604f0bbd0
Mduduzi Mlilo <MDUDUZI.MLILO@HOTMAIL.COM>
2023-01-14 17:06:51 +0200

    Some changes

--- final_project/machinetranslator/tests/tests.py
+++ final_project/machinetranslator/tests/tests.py
@@ -1,25 +1,21 @@
 import unittest
-<<<<<<<< HEAD:final_project/machinetranslator/tests/tests.py
-from final_project.machinetranslator.translator import english_to_french, french_to_english
-========
 from final_project.machinetranslation.translator import english_to_french, french_to_english
->>>>>>>> b6fb093 (package_folder_structure):final_project/machinetranslation/tests/tests.py
 
 
 class TestMethods(unittest.TestCase):
 
-    def testNullEnglishToFrench(self):
+    def test_NullEnglishToFrench(self):
         nullEnglish = english_to_french(None)
         self.assertIsNone(nullEnglish)
 
-    def testNullToFrenchEnglish(self):
+    def test_NullToFrenchEnglish(self):
         nullFrench = french_to_english(None)
         self.assertIsNone(nullFrench)
 
-    def testHelloEnglishToFrench(self):
+    def test_HelloEnglishToFrench(self):
         helloToFrench = english_to_french('Hello')
         self.assertEqual(helloToFrench, 'Bonjour')
 
-    def testBonjourToFrenchEnglish(self):
+    def test_BonjourToFrenchEnglish(self):
         bonjourInEnglish = french_to_english("Bonjour")
         self.assertEqual(bonjourInEnglish, 'Hello')
