f87d1193d55ee53a36bd4c9125d01e4270459e32
Mduduzi Mlilo <MDUDUZI.MLILO@HOTMAIL.COM>
2023-01-14 13:57:58 +0200

    e2f_translator_function

--- final_project/machinetranslator/translator.py
+++ final_project/machinetranslator/translator.py
@@ -9,15 +9,18 @@ load_dotenv()
 api_key = os.environ['apikey']
 api_url = os.environ['url']
 
+authenticator = IAMAuthenticator(api_key)
+
+language_translator = LanguageTranslatorV3(version='2018-05-01', authenticator=authenticator)
+language_translator.set_service_url(api_url)
+
+
+def englishToFrench(englishText):
+    translation = language_translator.translate(text=[englishText], model_id='en-fr').get_result()
+    frenchText = translation['translations'][0]['translation']
+    return frenchText
+
 
 if __name__ == "__main__":
     # Prepare the Authenticator
-    authenticator = IAMAuthenticator(api_key)
-    language_translator = LanguageTranslatorV3(version='2018-05-01', authenticator=authenticator)
-    language_translator.set_service_url(api_url)
-
-    # Translate
-    translation = language_translator.translate(text=['This is it', 'Something'], model_id='en-fr').get_result()
-
-    # Print results
-    print(json.dumps(translation, indent=2, ensure_ascii=False))
+    print(englishToFrench('I am doing just fine'))
